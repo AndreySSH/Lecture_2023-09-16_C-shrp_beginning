ecceee7f630c3865c6440d83f1b7be9f7db73c9f
Andrey <test@test>
2023-09-16 18:30:19 +0300

    add random

--- Examples/Example003_Sum/Program.cs
+++ Examples/Example003_Sum/Program.cs
@@ -1,8 +1,12 @@
 ﻿class Program{
     static void Main(string[] args){
-        int numberA = 3;
-        int numberB = 4;
+        int numberA = new Random().Next(1,10); //1,2...9
+        int numberB = new Random().Next(1,10);
         int result = numberA + numberB;
+        Console.Write(numberA);
+        Console.Write(" + "); 
+        Console.Write(numberB);
+        Console.Write(" = ");
         Console.WriteLine(result);
     }
 }
