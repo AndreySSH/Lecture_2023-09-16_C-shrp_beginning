72fc9cec22a2454a91104b1478b2437fc0dd092f
Andrey Shef <andrey.telnet@gmail.com>
2023-09-16 16:55:42 +0300

    add Example 002 Hello User project

---
+++ Examples/Example002_HelloUser/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program{
+    static void Main(string[] args){
+        Console.Write("Input your name: ");
+        string username = Console.ReadLine();
+        Console.Write("Hello, ");
+        Console.Write(username);
+        Console.WriteLine("!");
+    }
+}
