9f1bc09f041f671d9ca0f94a60dcdce29ed038bc
Andrey <test@test>
2023-09-16 18:07:18 +0300

    add Example 003 Hello Sum project

---
+++ Examples/Example003_Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿class Program{
+    static void Main(string[] args){
+        int numberA = 3;
+        int numberB = 4;
+        int result = numberA + numberB;
+        Console.WriteLine(result);
+    }
+}
