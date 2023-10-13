52c4b47f00f37771ad6f942b141d432c017ecdf8
Andrey <test@test>
2023-10-13 11:08:38 +0300

    added to example 13 recursion

--- Examples/Example013_RecursionAlgorithm/Program.cs
+++ Examples/Example013_RecursionAlgorithm/Program.cs
@@ -114,13 +114,94 @@
 // Рекурсивное нахождение чисел Фибоначчи
 /*-------------------------------------------------------------*/
 
-int Fibonacci(int num)
+// int Fibonacci(int num)
+// {
+//     // f(1) = 1
+//     // f(2) = 1
+//     if(num == 1 || num == 2) return 1;
+//     else return Fibonacci(num - 2) + Fibonacci(num - 1);
+// }
+
+// for(int i = 1; i < 11; i++)
+//     Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+
+/*-------------------------------------------------------------*/
+// Рекурсивная генерация всех слов заданной длинны, состоящих
+// из букв заданного алфавита
+/*-------------------------------------------------------------*/
+
+// int n = 1;
+// void FindWords(string alphabet, char[] word, int length = 0)
+// {
+//     if (length == word.Length)
+//     {
+//         Console.WriteLine($"{n++} {new String(word)}"); return;
+//     }
+//     for (int i = 0; i < alphabet.Length; i++)
+//     {
+//         word[length] = alphabet[i];
+//         FindWords(alphabet, word, length + 1);
+//     }
+// }
+
+// FindWords("аисв", new char[6]);
+
+/*-------------------------------------------------------------*/
+// Рекурсивный обход всех дирректорий относительно заданной и 
+// распечатка их содержимого
+/*-------------------------------------------------------------*/
+
+// using System.IO;
+
+// void CatalogInfo(string path, string indent = "")
+// {
+//     DirectoryInfo catalogs = new DirectoryInfo(path);
+//     foreach (var currentCatalog in catalogs.GetDirectories())
+//     {
+//         Console.WriteLine($"{indent}{currentCatalog.Name}");
+//         CatalogInfo(currentCatalog.FullName, indent + " ");
+//     }
+//     foreach (var item in catalogs.GetFiles())
+//     {
+//         Console.WriteLine($"{indent}{item.Name}");
+//     }
+// }
+// // string path = @"D:\_HOUSE\_geekbrains";
+// string path = @".";
+// CatalogInfo(path);
+
+/*-------------------------------------------------------------*/
+// Ханойские башни
+/*-------------------------------------------------------------*/
+
+// using System;
+
+// void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+// {
+//     if (count > 1) Towers(with, some, on, count - 1);
+//     Console.WriteLine($"{with} >> {on}");
+//     if (count > 1) Towers(some, on, with, count - 1);
+// }
+
+// Towers(count: 5);
+
+/*-------------------------------------------------------------*/
+// Обход структур
+/*-------------------------------------------------------------*/
+
+string emp = String.Empty;
+string[] tree = { emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3" };
+// 0 1 2 3 4 5 6 7 8 9 10 11
+void InOrderTraversal(int pos = 1)
 {
-    // f(1) = 1
-    // f(2) = 1
-    if(num == 1 || num == 2) return 1;
-    else return Fibonacci(num - 2) + Fibonacci(num - 1);
+    if (pos < tree.Length)
+    {
+        int left = 2 * pos;
+        int right = 2 * pos + 1;
+        if (left < tree.Length && !String.IsNullOrEmpty(tree[left])) InOrderTraversal(left);
+        Console.WriteLine(tree[pos]);
+        if (right < tree.Length && !String.IsNullOrEmpty(tree[right])) InOrderTraversal(right);
+    }
 }
 
-for(int i = 1; i < 11; i++)
-    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+InOrderTraversal(1);
